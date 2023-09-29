c702856aebc952ca26e16898b93a96259c8727f8
Wiktor Pabich <pabichwiktor@gmail.com>
2023-09-29 23:32:32 +0200

    feat: get all collections

--- src/gql/Query/collection.ts
+++ src/gql/Query/collection.ts
@@ -1,20 +1,23 @@
 import { Collection } from "@prisma/client";
 import { prisma } from "../../db";
 
-export const collection: Promise<Collection[]> = (
+export const collection = (
   _parent,
   _arg,
   _ctx,
-) => {
+): Promise<Collection[]> => {
 
-  return prisma.collection.findMany({
-    take: 1,
-    where: {
-      name: _arg.name
-    },
-    include: {
-      product: true,
-    }
-  });
+  if (_arg.name) {
+    return prisma.collection.findMany({
+      take: 1,
+      where: {
+        name: _arg.name
+      },
+      include: {
+        product: true,
+      }
+    });
+  }
 
+  return prisma.collection.findMany({});
 };
